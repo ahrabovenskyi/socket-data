68fd033e529acd3dc86c1de07a9f316d61d13a8e
Andrii Hrabovenskyi <ahrabovenskyi@ancestry.com>
2021-04-05 18:04:54 +0300

    init

---
+++ src/main/java/com/ahrabovenskyi/socketdata/config/WebSocketConfig.java
@@ -0,0 +1,15 @@
+package com.ahrabovenskyi.socketdata.config;
+
+import com.ahrabovenskyi.socketdata.handler.SocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new SocketHandler(), "/name").setAllowedOriginPatterns("*");
+    }
+}
